2359eaa465c63de09e0b9f461313ac5ab5e2ad17
Dave Kessler <activethrasher00@gmail.com>
2015-02-20 03:14:06 -0500

    ARM64: ARMv8 - aarch64 uses a hard float ABI
    Change-Id: I681bb1084147d87a5e20ed0a5e6296629c742dde

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,10 +1,12 @@
 # Configuration for Linux on ARM.
 # Generating binaries for the ARMv8-a architecture
 #
+
 ifneq (,$(filter denver64,$(TARGET_CPU_VARIANT)))
 
 arch_variant_cflags := \
     -mcpu=cortex-a57 \
-    -march=armv8-a
+    -march=armv8-a \
+    -mfloat-abi=hard
 
 endif
